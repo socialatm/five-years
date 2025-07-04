9345d385b772d9ef84555e58cc2d5932312d01a1
Ray <raypeaslee@gmail.com>
2025-07-04 07:52:18 -0400

    Create main.py
    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime, timedelta
+
+today = date.today()
+
+# Add a duration to a date
+future_date = today + timedelta(days=10)
+print(f"Date in 10 days: {future_date}")
+
+# five years ago
+five_years_ago = today - timedelta(days=365 * 5)
+print(f"Five years ago: {five_years_ago}")
