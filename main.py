d1c8ea37a5b9b7bfae35658ecd2c23a3c58d6069
Ray <raypeaslee@gmail.com>
2025-07-04 19:54:38 -0400

    Update main.py
    gets the last 5 years of fights

--- main.py
+++ main.py
@@ -20,3 +20,26 @@ events.columns = events.columns.str.lower()
 
 # save the events dataframe to a csv file named ufc_events.csv
 events.to_csv("ufc_events.csv", index=False)
+
+events.info()
+print(events.head())
+
+# load ufc_fight_details.csv into a DataFrame named fights
+fights = pd.read_csv("ufc_fight_details.csv")
+print(fights.head())
+fights.info()
+
+# only keep the rows where fight['EVENT'] isin events['event']
+fights = fights[fights['EVENT'].isin(events['event'])]
+
+# change all column names to lowercase
+fights.columns = fights.columns.str.lower()
+
+# save the fights dataframe to a csv file named ufc_fights.csv
+fights.to_csv("ufc_fights.csv", index=False)
+
+fights.info()
+print(fights.head())
+
+
+
