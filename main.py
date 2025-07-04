d7e2ac492ae3a8bd85019d28e3358ea6d1b6e982
Ray <raypeaslee@gmail.com>
2025-07-04 07:56:25 -0400

    Update main.py
    remove the original example

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from datetime import date, timedelta
 
 today = date.today()
 
-# Add a duration to a date
-future_date = today + timedelta(days=10)
-print(f"Date in 10 days: {future_date}")
-
 # five years ago
 five_years_ago = today - timedelta(days=365 * 5)
 print(f"Five years ago: {five_years_ago}")
