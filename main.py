2f52beb024d34b77b7ca7e9af49b19e19db73cc2
Ray <raypeaslee@gmail.com>
2025-07-04 18:02:09 -0400

    Update main.py
    clean up debug info

--- main.py
+++ main.py
@@ -5,28 +5,18 @@ today = date.today()
 
 # five years ago
 five_years_ago = today - timedelta(days=365 * 5)
-print(f"Five years ago: {five_years_ago}")
 
 # load ufc_event_details.csv into a DataFrame named events
 events = pd.read_csv("ufc_event_details.csv")
-print(events.head())
-
-# also print the info
-events.info()
 
 # change the dtype of the date column to datetime64
 events['DATE'] = pd.to_datetime(events['DATE'])
 
-#events.info()
-
-# drop all rows whose date is older thanfive_years_ago
-# events.drop(events[events['date'].dt.date < five_years_ago].index, inplace=True)
-
-# A more common and readable way to filter the DataFrame
+# only keep events that are 5 years old or newer
 events = events[events['DATE'].dt.date >= five_years_ago]
 
-events.info()
+# change all column names to lowercase
+events.columns = events.columns.str.lower()
 
 # save the events dataframe to a csv file named ufc_events.csv
 events.to_csv("ufc_events.csv", index=False)
-
